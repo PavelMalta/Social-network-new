efec9028f92b92414b978f6ecf58145f8dafd5a0
PavelMalta <shamashov87@mail.ru>
2021-02-21 21:54:54 +0300

    add HeaderContainer component in lesson 61

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Header.module.css';
+
 
 function Header() {
     return (
         <header className={s.header}>
             <img src={'https://i.pinimg.com/736x/c0/b3/09/c0b309c29f2e7bb5b7abcd51aa0bcec3.jpg'}/>
+
+            <div className={s.loginBlock}>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
         </header>)
 }
 export default Header;
---
+++ src/components/Header/HeaderContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css';
+import Header from "./Header";
+
+export class HeaderContainer extends React.Component<any, any> {
+
+    render() {
+        return (
+            <Header />
+            )
+    }
+}
+
