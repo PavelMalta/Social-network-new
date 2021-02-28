00a0afa261bf146f11e368ef7ff8e8a2446c8b6b
PavelMalta <shamashov87@mail.ru>
2021-02-28 21:44:15 +0300

    add getUsers in UsersContainer component, lesson 63

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -45,11 +45,10 @@ export class UsersComponent extends React.Component <UsersComponentPropsType> {
     onPageChanged = (pageNumber: number) => {
         this.props.setCurrentPage(pageNumber);
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
-        {withCredentials: true}
-        ).then(response => {
-            this.props.setUsers(response.data.items)
-            this.props.toggleIsFetching(false)
+        getUsers(pageNumber, this.props.pageSize)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.toggleIsFetching(false)
         })
     }
 
