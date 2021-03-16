8cf017e9db4c9da7172900ca299d94411f1c5400
PavelMalta <shamashov87@mail.ru>
2021-03-16 17:08:22 +0300

    add Form component, lesson 75

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -6,6 +6,32 @@ type LoginPropsType = {
 
 export const Login = (props: LoginPropsType) => {
     return (
-        <h1>Login</h1>
+        <div>
+            <h1>Login</h1>
+           <Form />
+        </div>
+    )
+}
+
+export type FormType = {
+
+}
+
+export const Form = (props: FormType) => {
+    return (
+        <form>
+            <div>
+                <input placeholder={'Login'}/>
+            </div>
+            <div>
+                <input placeholder={'Password'}/>
+            </div>
+            <div>
+                <input type={'checkbox'}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     )
 }
