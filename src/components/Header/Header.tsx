a45ca39365b44fd7c80279db8c5ad2dcb54dba3f
PavelMalta <shamashov87@mail.ru>
2021-04-15 16:27:05 +0300

    add logout logic in Header component, lesson 78

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { logoutTC } from "../../redux/auth-reducer";
 import s from './Header.module.css';
 import {HeaderComponentPropsType} from "./HeaderContainer";
+import {useDispatch} from "react-redux";
 
 
 function Header(props: HeaderComponentPropsType) {
+    const dispatch = useDispatch()
+
+    const logOut = () => {
+        return dispatch(logoutTC())
+    }
     return (
         <header className={s.header}>
             <img src={'https://i.pinimg.com/736x/c0/b3/09/c0b309c29f2e7bb5b7abcd51aa0bcec3.jpg'}/>
 
             <div className={s.loginBlock}>
-                { props.isAuth ?
-                    props.login :
-                    <NavLink to={'/login'}>Login</NavLink>}
+                { props.isAuth
+                    ? <div>{ props.login} - <button onClick={logOut}>Log out</button> </div>
+                    : <NavLink to={'/login'}>Login</NavLink>}
             </div>
         </header>)
 }
