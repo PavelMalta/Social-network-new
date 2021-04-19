2446062778e40919afafea7432a0a0ae32354709
PavelMalta <shamashov87@mail.ru>
2021-04-19 16:43:22 +0300

    add initializedApp thunk in app-reducer, lesson 80

--- src/redux/app-reducer.ts
+++ src/redux/app-reducer.ts
@@ -2,6 +2,7 @@ import {authAPI} from "../api/api";
 import {Dispatch} from "redux";
 import {toggleIsFetching} from "./users-reducer";
 import {stopSubmit} from "redux-form";
+import {ActionsUsersTypes, getAuthUserData} from "./auth-reducer";
 
 const SET_INITIALIZED = 'SET-INITIALIZED'
 
@@ -25,44 +26,15 @@ export const appReducer = (state: stateType = initialState, action: ActionsAppTy
 
 export const setInitialized = () => ({type: SET_INITIALIZED})
 
-// export const getAuthUserData = () => {
-//     return (dispatch: Dispatch<ActionsUsersTypes>) => {
-//         authAPI.me()
-//             .then(data => {
-//                 if (data.resultCode === 0) {
-//                     dispatch(setAuthUserData(data.data.id, data.data.login, data.data.email, true))
-//                 }
-//             })
-//     }
-// }
-//
-// export const loginTC = (email: string, password: string, rememberMe: boolean) => {
-//     return (dispatch: Dispatch<ActionsUsersTypes>) => {
-//         dispatch(toggleIsFetching(true))
-//         authAPI.login(email, password, rememberMe)
-//             .then(data => {
-//                     if (data.resultCode === 0) {
-//                         dispatch(setAuthUserData(data.data.id, data.data.login, data.data.email, true))
-//                     } else {
-//                         let message = data.messages.length > 0 ? data.messages[0] : "Some error"
-//                         // @ts-ignore
-//                         dispatch(stopSubmit('login', {_error: message}))
-//                     }
-//                 }
-//             )
-//             .finally(() => {
-//                 dispatch(toggleIsFetching(false))
-//             })
-//     }
-// }
-// export const logoutTC = () => {
-//     return (dispatch: Dispatch<ActionsUsersTypes>) => {
-//         authAPI.logout()
-//             .then(data => {
-//                     if (data.resultCode === 0) {
-//                         dispatch(setAuthUserData(null, null, null, false))
-//                     }
-//                 }
-//             )
-//     }
-// }
+
+export const initializedApp = () => {
+     return (dispatch: any) => {
+           let dispatchResult = dispatch(getAuthUserData())
+         dispatchResult.then(() => {
+             dispatch(setInitialized())
+         })
+
+     }}
+
+type thunkDispatch = Dispatch<ActionsAppTypes | ActionsUsersTypes>
+
